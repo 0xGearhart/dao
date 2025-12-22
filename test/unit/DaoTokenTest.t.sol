ea2790c73a880072c173bdee157950a2308b15d2
gearhart <geardev26@gmail.com>
2025-12-22 01:49:24 -0800

    setup

--- test/unit/DaoTokenTest.t.sol
+++ test/unit/DaoTokenTest.t.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 
+import {CodeConstants} from "../../script/HelperConfig.s.sol";
 import {DaoToken} from "../../src/DaoToken.sol";
 import {Test} from "forge-std/Test.sol";
 
@@ -7,6 +8,9 @@ pragma solidity ^0.8.27;
 
 contract DaoTokenTest is Test {
     DaoToken public daoToken;
+    address USER = makeAddr("user");
 
-    function setUp() public {}
+    function setUp() public {
+        daoToken = new DaoToken(TOKEN_NAME, TOKEN_SYMBOL, INITIAL_SUPPLY);
+    }
 }
